7ca986194611ca4ce37425fc828beca53f8b20b4
AnkA852456 <108530192+AnkA852456@users.noreply.github.com>
2022-11-09 19:57:18 +0200

    HW8_1_task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,39 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Change code below this line
+const galleryMarkup = makeGalleryMarkup(galleryItems);
+
+const refs = {
+  galleryEl: document.querySelector('.gallery'),
+};
+
+refs.galleryEl.insertAdjacentHTML('beforeend', galleryMarkup);
+
+new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+});
+
+refs.galleryEl.addEventListener('click', onGalleryClick);
+
+function makeGalleryMarkup(gallery) {
+  return gallery
+    .map(({ original, preview, description }) => {
+      return `
+        <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="" title= "${description}"/></a>`;
+    })
+    .join('');
+}
+
+function onGalleryClick(evt) {
+  evt.preventDefault();
+  const isOnImage = evt.target.classList.contains('gallery__image');
+  if (!isOnImage) {
+    return;
+  }
+}
 
 console.log(galleryItems);
